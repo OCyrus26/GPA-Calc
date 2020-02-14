cde05b995b042ea9d74cce21b63f04dd5900abf8
O'Cyrus JB <s1291542@monmouth.edu>
2020-02-13 21:12:20 -0500

    version1

---
+++ src/gpa.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+public class gpa {
+
+	public static void main(String[] args) 
+	{
+		
+			
+		
+		Scanner scan = new Scanner (System.in);
+		
+		String letterGrade = " ";
+		double gradePoint = 0;
+		
+		
+			
+	
+		
+		while (!letterGrade.equalsIgnoreCase("Q")) 
+		{
+			System.out.println("Enter grade: ");
+			letterGrade = scan.next();
+		switch (letterGrade) 
+		{
+		
+		case "A" : gradePoint = 4.00;
+			
+		break;
+		
+		case "A-" : gradePoint = 3.70;
+		
+		break;
+		
+		case "B+" : gradePoint = 3.33;
+		
+		break;
+		
+		case "B" : gradePoint = 3.00;
+		
+		break;
+		
+		case "B-" : gradePoint = 2.70;
+		
+		break;
+		
+		case "C+" : gradePoint = 2.30;
+
+		break;
+
+		case "C" : gradePoint = 2.00;
+
+		break;
+
+		case "C-" : gradePoint = 1.70;
+
+		break;
+
+		case "D+" : gradePoint = 1.30;
+
+		break;
+		
+		case "D" : gradePoint = 1.00;
+
+		break;
+		
+		case "D-" : gradePoint = 0.70;
+
+		break;
+		
+		case "F" : gradePoint = 0.00;
+
+		break;
+		
+		
+		default: 
+		}
+		
+		if (!letterGrade.equalsIgnoreCase("Q")) 
+		{
+			System.out.println("Your grade point is: "+gradePoint);
+		}
+		
+		else 
+		{
+			System.out.println("End Program");
+
+		}
+		} 
+		
+	}
+
+}
